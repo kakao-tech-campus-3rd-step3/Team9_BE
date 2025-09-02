c63c560d254cc622597d281610f2d8f53534898a
HJH <jijinny26@gmail.com>
2025-09-03 06:00:07 +0900

    docs(user): 유저 관련 API 문서 추가

---
+++ src/main/java/com/pado/domain/user/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.pado.domain.user.controller;
+
+import com.pado.domain.user.dto.UserDetailResponseDto;
+import com.pado.domain.user.dto.UserSimpleResponseDto;
+import com.pado.global.swagger.annotation.user.Api404UserNotFoundError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@Tag(name = "02. User", description = "인증된 사용자 정보 관련 API")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    // TODO: 서비스 레이어 종속성 주입
+
+    @Api404UserNotFoundError
+    @Operation(summary = "유저 정보 조회", description = "인증 토큰을 통해 현재 로그인된 사용자의 주요 정보(닉네임, 이미지 URL)를 조회합니다.")
+    @ApiResponse(
+            responseCode = "200", description = "유저 정보 조회 성공",
+            content = @Content(schema = @Schema(implementation = UserSimpleResponseDto.class))
+    )
+    @GetMapping
+    public ResponseEntity<UserSimpleResponseDto> getSimpleUserInfo() {
+        // TODO: 사용자 주요 정보 조회 로직 구현
+        return ResponseEntity.ok(new UserSimpleResponseDto("파도", "https://s3.image.url/profile.jpg"));
+    }
+
+    @Api404UserNotFoundError
+    @Operation(summary = "유저 세부 정보 조회", description = "인증 토큰을 통해 현재 로그인된 사용자의 세부 정보(관심분야, 지역 포함)를 조회합니다.")
+    @ApiResponse(
+            responseCode = "200", description = "세부 정보 조회 성공",
+            content = @Content(schema = @Schema(implementation = UserDetailResponseDto.class))
+    )
+    @GetMapping("/detail")
+    public ResponseEntity<UserDetailResponseDto> getDetailUserInfo() {
+        // TODO: 사용자 세부 정보 조회 로직 구현
+        UserDetailResponseDto mockResponse = new UserDetailResponseDto(
+                "파도",
+                "https://s3.image.url/profile.jpg",
+                Arrays.asList("프로그래밍", "취업"),
+                "서울"
+        );
+        return ResponseEntity.ok(mockResponse);
+    }
+}
