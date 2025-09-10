57be9dddc2a6c1a4ed10861504637f62757ae1cc
user <cyg0828@gmail.com>
2025-09-11 00:30:35 +0900

    feat(user-domain): user 엔티티 수정 및 userInterest 엔티티, userRepository 추가
    User와 UserInterest 엔티티 간 1:N 매핑 추가
    userRepository 추가

--- src/main/java/com/pado/domain/user/entity/User.java
+++ src/main/java/com/pado/domain/user/entity/User.java
@@ -1,5 +1,9 @@
 package com.pado.domain.user.entity;
 
+import com.pado.domain.shared.entity.Category;
+import com.pado.domain.shared.entity.Region;
+import com.pado.global.exception.common.BusinessException;
+import com.pado.global.exception.common.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -8,6 +12,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,8 +34,9 @@ public class User {
     @Column(nullable = false, unique = true, length = 100)
     private String nickname;
 
+    @Enumerated(EnumType.STRING)
     @Column(length = 100)
-    private String region;
+    private Region region;
 
     @Column(length = 500)
     private String profileImageUrl;
@@ -38,6 +45,9 @@ public class User {
     @Column(nullable = false)
     private Gender gender;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserInterest> interests = new ArrayList<>();
+
     @CreationTimestamp
     private LocalDateTime createdAt;
 
@@ -45,7 +55,7 @@ public class User {
     private LocalDateTime updatedAt;
 
     @Builder
-    public User(String email, String passwordHash, String nickname, String region, String profileImageUrl, Gender gender){
+    public User(String email, String passwordHash, String nickname, Region region, String profileImageUrl, Gender gender){
         this.email = email;
         this.passwordHash = passwordHash;
         this.nickname = nickname;
@@ -54,12 +64,24 @@ public class User {
         this.gender = gender;
     }
 
+    public void addInterest(Category category){
+        if (category == null) throw new IllegalArgumentException("category는 필수입니다.");
+        boolean exists = this.interests.stream()
+                .anyMatch(ui -> ui.getCategory() == category);
+        if (exists) throw new BusinessException(ErrorCode.DUPLICATE_INTEREST);
+        UserInterest interest = UserInterest.builder()
+                .user(this)
+                .category(category)
+                .build();
+        this.interests.add(interest);
+    }
+
 
     public void changeNickname(String nickname) {
         this.nickname = nickname;
     }
 
-    public void changeRegion(String region) {
+    public void changeRegion(Region region) {
         this.region = region;
     }
 
