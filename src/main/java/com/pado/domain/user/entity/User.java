e55dc7486317d0918f1688c2b187cba894deed27
user <cyg0828@gmail.com>
2025-09-11 17:47:45 +0900

    feat(user): 유저 정보 조회, 유저 세부정보 조회 추가
    - 일반 사용자 정보 조회 컨트롤러 메서드 구현
    - 사용자 세부 정보 조회 시 interests 컬렉션 fetch join으로 로딩

--- src/main/java/com/pado/domain/user/entity/User.java
+++ src/main/java/com/pado/domain/user/entity/User.java
@@ -10,9 +10,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import java.time.LocalDateTime;
+
 import java.util.ArrayList;
 import java.util.List;
 
