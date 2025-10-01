1039ce49b451db7923e7924335074a8cffde8792
seongwon02 <seongwon2303@gmail.com>
2025-10-01 21:19:55 +0900

    feat(chat): 마지막으로 읽은 메세지 레포지토리 생성 및 않읽은 메세지 알림에 사용되는 메서드 추가

---
+++ src/main/java/com/pado/domain/chat/repository/LastReadMessageRepository.java
@@ -0,0 +1,14 @@
+package com.pado.domain.chat.repository;
+
+import com.pado.domain.chat.entity.LastReadMessage;
+import com.pado.domain.study.entity.StudyMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface LastReadMessageRepository extends JpaRepository<LastReadMessage, Long> {
+    
+    Optional<LastReadMessage> findByStudyMember(StudyMember studyMember);
+}
