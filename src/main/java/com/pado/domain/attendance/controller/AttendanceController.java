e78910db685588d84caaf36110223b141780835e
cyg0828 <cyg0828@gmail.com>
2025-10-02 07:59:05 +0900

    fix(attendance): 참여 여부 조회, 수정, 추가 api 구현
    - 특정 스케줄 스터디원 참여 여부 조회 api 구현
    - 특정 스케줄 스터디원 참여 여부 수정 api 구현
    - 출석 로직에서 참여 여부 로직으로 변경

--- src/main/java/com/pado/domain/attendance/controller/AttendanceController.java
+++ src/main/java/com/pado/domain/attendance/controller/AttendanceController.java
@@ -1,9 +1,6 @@
 package com.pado.domain.attendance.controller;
 
-import com.pado.domain.attendance.dto.AttendanceListResponseDto;
-import com.pado.domain.attendance.dto.AttendanceStatusDto;
-import com.pado.domain.attendance.dto.AttendanceStatusResponseDto;
-import com.pado.domain.attendance.dto.MemberAttendanceDto;
+import com.pado.domain.attendance.dto.*;
 import com.pado.domain.attendance.service.AttendanceService;
 import com.pado.domain.user.entity.User;
 import com.pado.global.auth.annotation.CurrentUser;
@@ -24,11 +21,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
-@Tag(name = "10. Attendance", description = "출석 관리 관련 API")
+@Tag(name = "14. Attendance", description = "출석 관리 관련 API")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -49,7 +47,7 @@ public class AttendanceController {
     @Parameters({
         @Parameter(name = "study_id", description = "참여 현황을 조회할 스터디의 ID", required = true, example = "1")
     })
-    @GetMapping("/studies/{study_id}/attendance")
+    @GetMapping("/studies/{study_id}/attendances")
     public ResponseEntity<AttendanceListResponseDto> getFullAttendance(
         @PathVariable("study_id") Long studyId
     ) {
@@ -69,51 +67,80 @@ public class AttendanceController {
     @Parameters({
         @Parameter(name = "schedule_id", description = "출석 상태를 조회할 일정의 ID", required = true, example = "1234")
     })
-    @GetMapping("schedules/{scheduleid}/attendance")
-    public ResponseEntity<AttendanceStatusResponseDto> getIndividualAttendanceStatus(
-        @PathVariable("scheduleid") Long scheduleId,
+    @GetMapping("/schedules/{schedule_id}/me")
+    public ResponseEntity<AttendanceStatusResponseDto> getMyAttendanceStatus(
+        @PathVariable("schedule_id") Long scheduleId,
         @Parameter(hidden = true) @CurrentUser User user
     ) {
-        return ResponseEntity.ok(attendanceService.getIndividualAttendanceStatus(scheduleId, user));
+        return ResponseEntity.ok(attendanceService.getMyAttendanceStatus(scheduleId, user));
+    }
+
+    @Api403ForbiddenStudyMemberOnlyError
+    @Api404StudyNotFoundError
+    @Operation(
+            summary = "특정 스케줄 참여 현황 조회",
+            description = "특정 스케줄의 스터디원 참여 현황을 조회합니다. (스터디 리더만 가능)"
+    )
+    @ApiResponse(
+            responseCode = "200", description = "전체 참여 현황 조회 성공",
+            content = @Content(schema = @Schema(implementation = AttendanceListResponseDto.class))
+    )
+    @Parameters({
+            @Parameter(name = "study_id", description = "참여 현황을 조회할 스터디의 ID", required = true, example = "1")
+    })
+    @GetMapping("/studies/{study_id}/attendances/{schedule_id}")
+    public ResponseEntity<AttendanceListResponseDto> getScheduleAttendance(
+            @PathVariable("study_id") Long studyId,
+            @PathVariable("schedule_id") Long scheuleId,
+            @Parameter(hidden = true) @CurrentUser User user
+    ) {
+        return ResponseEntity.ok(attendanceService.getScheduleAttendance(studyId, scheuleId, user));
     }
 
     @Api403ForbiddenStudyMemberOnlyError
     @Api404ScheduleNotFoundError
     @Operation(
-        summary = "출석 체크",
-        description = "스터디원이 자신의 출석을 체크합니다."
+            summary = "참석 여부 수정",
+            description = "스터디 리더가 스터디원의 참석 여부를 수정합니다."
     )
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "출석 체크 성공",
-            content = @Content(schema = @Schema(implementation = AttendanceStatusResponseDto.class))),
-        @ApiResponse(responseCode = "409", description = "출석 시간 만료 또는 이미 출석 체크됨",
-            content = @Content(
-                mediaType = "application/json",
-                schema = @Schema(implementation = ErrorResponseDto.class),
-                examples = {
-                    @ExampleObject(
-                        name = "이미 출석 체크됨 예시",
-                        value = """
-                            {
-                              "code": "ALREADY_CHECKED_IN",
-                              "message": "이미 출석 체크되었습니다.",
-                              "errors": [],
-                              "timestamp": "2025-09-07T08:15:10.8668626",
-                              "path": "/api/schedules/1234/attendance"
-                            }
-                            """
-                    )
-                })
-        )
+            @ApiResponse(responseCode = "200", description = "참석 수정 성공",
+                    content = @Content(schema = @Schema(implementation = AttendanceStatusResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다.")
     })
     @Parameters({
-        @Parameter(name = "schedule_id", description = "출석 체크할 일정의 ID", required = true, example = "1234")
+            @Parameter(name = "schedule_id", description = "참석 여부 수정할 일정의 ID", required = true, example = "1234")
     })
-    @PostMapping("/schedules/{schedule_id}/attendance")
-    public ResponseEntity<AttendanceStatusResponseDto> checkAttendance(
+    @PatchMapping("/schedules/{schedule_id}/attendances")
+    public ResponseEntity<AttendanceStatusResponseDto> changeAttendance(
+            @PathVariable("schedule_id") Long scheduleId,
+            @RequestBody AttendanceMemberStatusRequestDto attendanceMemberStatusRequestDto,
+            @Parameter(hidden = true) @CurrentUser User user
+    ) {
+        attendanceService.changeMemberAttendanceStatus(scheduleId, attendanceMemberStatusRequestDto, user);
+        return ResponseEntity.status(HttpsURLConnection.HTTP_OK).build();
+    }
+
+    @Api403ForbiddenStudyMemberOnlyError
+    @Api404ScheduleNotFoundError
+    @Operation(
+        summary = "참석 여부 추가",
+        description = "스터디원이 자신의 참석 여부를 추가합니다."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "참석 추가 성공",
+            content = @Content(schema = @Schema(implementation = AttendanceStatusResponseDto.class)))
+    })
+    @Parameters({
+        @Parameter(name = "schedule_id", description = "참석 여부 추가할 일정의 ID", required = true, example = "1234")
+    })
+    @PatchMapping("/schedules/{schedule_id}/attendances/me")
+    public ResponseEntity<Void> addAttendance(
         @PathVariable("schedule_id") Long scheduleId,
+        @RequestBody AttendanceStatusRequestDto  attendanceStatusRequestDto,
         @Parameter(hidden = true) @CurrentUser User user
     ) {
-        return ResponseEntity.ok(attendanceService.checkIn(scheduleId, user));
+        attendanceService.changeMyAttendanceStatus(scheduleId, attendanceStatusRequestDto, user);
+        return ResponseEntity.status(HttpsURLConnection.HTTP_OK).build();
     }
 }
