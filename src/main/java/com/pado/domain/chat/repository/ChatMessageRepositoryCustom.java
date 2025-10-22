66a6f5f0e269657bc7e0748e9115f81d097f623d
user <cyg0828@gmail.com>
2025-10-22 18:48:58 +0900

    fix: 안쓰는 import문 제거

--- src/main/java/com/pado/domain/chat/repository/ChatMessageRepositoryCustom.java
+++ src/main/java/com/pado/domain/chat/repository/ChatMessageRepositoryCustom.java
@@ -5,7 +5,5 @@ import com.pado.domain.chat.entity.ChatMessage;
 import java.util.List;
 
 public interface ChatMessageRepositoryCustom {
-    
-
     List<ChatMessage> findChatMessagesWithCursor(Long studyId, Long cursor, int size);
 }
